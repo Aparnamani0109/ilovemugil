c36a4160ed11fc2c78edc9aa7fbeb94f0648c144
aparnamaparna <aparnamani1999@gmail.com>
2026-02-13 22:24:02 +0530

    adding all code

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/ilovemugil/', // Set base path for GitHub Pages
   plugins: [react()],
   build: {
     outDir: 'dist',
